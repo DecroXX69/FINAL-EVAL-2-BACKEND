28a14513032c69aea6d5f95db3a85ad5f2490176
DecroXX <rushikeshpalav23@gmail.com>
2024-12-04 11:45:49 +0530

    package.json updated

--- Routes/foodItemRoutes.js
+++ Routes/foodItemRoutes.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const foodItemController = require('../controllers/fooditemController');
+const foodItemController = require('../controllers/foodItemController');
 const { protect } = require('../middleware/AuthMiddleware');
 
 router.get('/food-items', protect, foodItemController.getAllFoodItems);
