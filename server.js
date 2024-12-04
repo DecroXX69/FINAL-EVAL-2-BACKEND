3eaab9efa250319018b429ff1e3520c5c587b980
DecroXX <rushikeshpalav23@gmail.com>
2024-12-04 10:26:33 +0530

    new

--- server.js
+++ server.js
@@ -17,14 +17,7 @@ const app = express();
 
 
 app.use(express.json());
-app.use(
-  cors({
-    origin: 'https://final-eval-2-frontend.vercel.app',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  })
-);
+app.use(cors());
 
 
 mongoose
